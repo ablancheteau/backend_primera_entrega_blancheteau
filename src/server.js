01e274cb3e6552868b4449d6e0966d4c04cdde77
alexander blancheteau <alexblanch22@gmail.com>
2024-06-13 18:05:18 -0400

    Se creo un sistema de login

--- src/server.js
+++ src/server.js
@@ -4,14 +4,25 @@ import morgan from 'morgan';
 import productsRouter from './routes/product.router.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import 'dotenv/config'
+import loginRouter from './routes/login.router.js';
+import viewsRouter from './routes/views.router.js';
+import cookieParser from 'cookie-parser';
+import { SECRET } from './utils.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
+app.use(cookieParser(SECRET))
 
 app.use('/products', productsRouter);
+app.use('/login', loginRouter);
+app.use('/', viewsRouter);
+
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
 
 app.use(errorHandler);
 
